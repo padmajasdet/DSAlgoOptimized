b129f9e52a6885560c9d8f8f3f54134e4a20b3fe
padma <padma@LAPTOP-LQMIT81G.mynetworksettings.com>
2024-05-30 16:21:50 -0400

    updated

--- src/test/java/PageObjects/PageObjectManager.java
+++ src/test/java/PageObjects/PageObjectManager.java
@@ -15,7 +15,13 @@ public class PageObjectManager {
  RegistrationPage registrationpage;
  ArrayPage arrayPage;
  GraphPage graphPage;
- 
+ QueuePage queuePage;
+ //LinkedListPage linkedListPage;
+ //TryEditorPage tryEditorPage;
+ //DSIntroductionPage dsIntroductionPage;
+ //HomePage homePage;
+ //TreePage treePage;
+ //StackPage stackPage;
 	public PageObjectManager(WebDriver driver) {
 		this.driver=driver;
 	}
@@ -49,5 +55,35 @@ public class PageObjectManager {
 		return graphPage;
 			
 	}
-	
-}
+	public QueuePage getQueuePage() {
+		queuePage =new QueuePage(driver);
+		return queuePage;
+			
+	}
+//	public LinkedListPage getLinkedListPage() {
+//		linkedListPage = new LinkedListPage(driver);
+//		return linkedListPage;
+//	}
+//	public HomePage getHomePage() {
+//		homePage = new HomePage(driver);
+//		return homePage;
+//	}
+//	public TryEditorPage getTryEditorPage() {
+//		tryEditorPage = new TryEditorPage(driver);
+//		return tryEditorPage;
+//	}
+//	public DSIntroductionPage getDsIntroductionPage() {
+//		dsIntroductionPage = new DSIntroductionPage(driver);
+//		return dsIntroductionPage;
+//	}
+//	
+//	public TreePage getTreePage() {
+//		treePage =new TreePage(driver);
+//		return treePage;
+//			
+	}
+//	public StackPage getStackPage() {
+//		stackPage = new StackPage(driver);
+//		return stackPage;
+//	}
+
